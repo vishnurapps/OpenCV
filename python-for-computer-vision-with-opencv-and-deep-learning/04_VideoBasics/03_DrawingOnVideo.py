92e6435d2cb617b3b3838d99931e24c7696c0d44
Vishnu R <vishnurapps@gmail.com>
2019-01-23 07:59:47 +0530

    Drawing on live video

--- python-for-computer-vision-with-opencv-and-deep-learning/04_VideoBasics/03_DrawingOnVideo.py
+++ python-for-computer-vision-with-opencv-and-deep-learning/04_VideoBasics/03_DrawingOnVideo.py
@@ -1,28 +1,43 @@
 import cv2
 
+#Callback function
+def draw_rect(event, x, y, flags, params):
+    global pt1, pt2, topLeftClicked, bottomRightClicked
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Reset the rectangle
+        if topLeftClicked == True and bottomRightClicked == True:
+            pt1 = (0,0)
+            pt2 = (0,0)
+            topLeftClicked = False
+            bottomRightClicked = False
+        
+        if topLeftClicked == False:
+            pt1 = (x,y)
+            topLeftClicked = True
+        elif bottomRightClicked == False:
+            pt2 = (x,y)
+            bottomRightClicked = True
+            
+
+#Global variable
+pt1 = (0,0)
+pt2 = (0,0)
+topLeftClicked = False
+bottomRightClicked = False
+
+#Connect to callback
 cap = cv2.VideoCapture(0)
+cv2.namedWindow('Test')
+cv2.setMouseCallback('Test', draw_rect)
 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-# TOP CORNER
-x = width // 2
-y = height //2
-
-# width and height of rectangle
-w = width // 4
-h = height // 4
-
-#bottom right corner is x+w and y+w
 while True:
     ret, frame = cap.read()
-    
-    cv2.rectangle(frame, (x,y), (x+w,y+w), color=(0,0,255), thickness=4)
-    cv2.imshow('frame', frame)
-
-    #Color capture
-    
-    cv2.imshow('frame', frame)
+    #Drawing based on global variables
+    if topLeftClicked:
+        cv2.circle(frame, center=pt1, radius=5, color=(0,0,255),thickness=-1)
+    if topLeftClicked and bottomRightClicked:
+        cv2.rectangle(frame, pt1, pt2, color=(0,0,255), thickness=1)
+    cv2.imshow('Test', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
