31a0ee2a822f90c8f2962a3200f34e1d9a934384
Vishnu R <vishnurapps@gmail.com>
2019-01-27 22:15:42 +0530

    Face detection in video

---
+++ python-for-computer-vision-with-opencv-and-deep-learning/05_ObjectTracking/10_VideoFaceDetection.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2 
+import matplotlib.pyplot as plt
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+def detect_face(img):
+    
+  
+    face_img = img.copy()
+  
+    face_rects = face_cascade.detectMultiScale(face_img) 
+    
+    for (x,y,w,h) in face_rects: 
+        cv2.rectangle(face_img, (x,y), (x+w,y+h), (255,0,0), 10) 
+        
+    return face_img
+
+cap = cv2.VideoCapture(0) 
+
+while True: 
+    
+    ret, frame = cap.read(0) 
+     
+    frame = detect_face(frame)
+ 
+    cv2.imshow('Video Face Detection', frame) 
+ 
+    c = cv2.waitKey(1) 
+    if c == 27: 
+        break 
+        
+cap.release() 
+cv2.destroyAllWindows()
